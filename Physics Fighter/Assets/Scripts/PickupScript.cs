80a50552519d86223ab8c64944df654288451e5b
ThomasBudden <buddenthomas04@gmail.com>
2026-01-08 10:07:26 +0000

    Some changes

--- Physics Fighter/Assets/Scripts/PickupScript.cs
+++ Physics Fighter/Assets/Scripts/PickupScript.cs
@@ -1,17 +1,21 @@
 using NUnit.Framework;
-using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.UI.Image;
 
 public class PickupScript : MonoBehaviour
 {
-    public RaycastHit2D[] objects;
+    public Collider2D[] objects;
     public bool selecting;
-    public float distance;
+    public float distance = 100;
     public GameObject closest;
     public GameObject target;
     public Rigidbody2D rb;
     public float targetDisX;
     public float targetDisY;
+    public LayerMask castLayerMask;
+    public Vector2 worldPosition;
+    public float targetAngle;
     void Start()
     {
         selecting = true;
@@ -19,21 +23,21 @@ public class PickupScript : MonoBehaviour
 
     void Update()
     {
+        worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         if (Input.GetMouseButtonDown(1) == true && selecting == true)
         {
-            objects = Physics2D.CircleCastAll(Input.mousePosition, 0.1f, new Vector2(0, 0), 0);
+            objects = Physics2D.OverlapCircleAll(worldPosition, 1f,castLayerMask);
             {
-                for (int i = 0; i < objects.Length; i++)
+                
+                if(objects.Length>=0)
                 {
-                    if (i == 0)
-                    {
-                        distance = Vector2.Distance(Input.mousePosition, objects[i].transform.position);
-                        closest = objects[i].collider.gameObject;
-                    }
-                    else if (i != 0 && Vector2.Distance(Input.mousePosition, objects[i].transform.position) < distance)
-                    {
-                        distance = Vector2.Distance(Input.mousePosition, objects[i].transform.position);
-                        closest = objects[i].collider.gameObject;
+                    for (int i = 0; i < objects.Length; i++)
+                    {                
+                        if(distance> Vector2.Distance(worldPosition, objects[i].transform.position))
+                        { 
+                            closest = objects[i].gameObject; 
+                        }
+
                     }
                 }
             }
@@ -46,9 +50,18 @@ public class PickupScript : MonoBehaviour
         if (selecting == false && target != null)
         {
             rb = target.GetComponent<Rigidbody2D>();
-            targetDisX = Input.mousePosition.x - target.transform.position.x;
-            targetDisY = Input.mousePosition.y - target.transform.position.y;
-            rb.AddForce(new Vector2(targetDisX * Time.deltaTime, targetDisY * Time.deltaTime));
+          /*  targetDisX = worldPosition.x - target.transform.position.x;
+            targetDisY = worldPosition.y - target.transform.position.y;
+            targetAngle = Vector2.Angle(new Vector2(worldPosition.x, target.transform.position.x), new Vector2(worldPosition.y, target.transform.position.y));
+            Debug.Log(targetAngle);
+            rb.AddForce(new Vector2(targetDisX * Time.deltaTime, targetDisY * Time.deltaTime));*/
+
+            Vector2 direction = new Vector2(target.transform.position.x , target.transform.position.y) - worldPosition;
+            float signedAngleRad = Mathf.Atan2(direction.y, direction.x);
+            float signedAngleDeg = signedAngleRad * Mathf.Rad2Deg;
+            Debug.Log(signedAngleDeg);
+
+
         }
     }
 }
